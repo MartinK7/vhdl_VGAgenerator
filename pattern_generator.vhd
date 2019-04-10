6ea08ec0262bcf06d3632c4caaa550bf0dec6011
MartinK7 <unicorn.light.bulb@gmail.com>
2019-04-10 10:54:14 +0200

    pattern_update

--- pattern_generator.vhd
+++ pattern_generator.vhd
@@ -32,10 +32,51 @@ end pattern_generator;
 -- Architecture declaration for pattern generator
 --------------------------------------------------------------------------------
 architecture Behavioral of pattern_generator is
-	-- nil
+	signal red_next_o_1 : std_logic_vector(3-1 downto 0);
+	signal green_next_o_1 : std_logic_vector(3-1 downto 0);
+	signal blue_next_o_1 : std_logic_vector(2-1 downto 0);
+	
+	signal red_next_o_2 : std_logic_vector(3-1 downto 0);
+	signal green_next_o_2 : std_logic_vector(3-1 downto 0);
+	signal blue_next_o_2 : std_logic_vector(2-1 downto 0);
+	
+	signal red_next_o_3 : std_logic_vector(3-1 downto 0);
+	signal green_next_o_3 : std_logic_vector(3-1 downto 0);
+	signal blue_next_o_3 : std_logic_vector(2-1 downto 0);
+	
+	signal red_next_o_4 : std_logic_vector(3-1 downto 0);
+	signal green_next_o_4 : std_logic_vector(3-1 downto 0);
+	signal blue_next_o_4 : std_logic_vector(2-1 downto 0);
 begin
 	
-	red_next_o <= "111" when (x_pos_next_i=0 and y_pos_next_i=0) else selpat_i(7 downto 5);
-	green_next_o <= "111" when (x_pos_next_i=640-1 and y_pos_next_i=480-1) else selpat_i(4 downto 2);
-	blue_next_o <=	selpat_i(1 downto 0);
+	 red_next_o_1 <= "111" when (x_pos_next_i=0 and y_pos_next_i=0) else selpat_i (7 downto 5);
+	 green_next_o_1 <= "111" when (x_pos_next_i=640-1 and y_pos_next_i=480-1) else selpat_i (4 downto 2);
+	 blue_next_o_1 <=	selpat_i(1 downto 0);
+
+    red_next_o_2 <= "111" when ((x_pos_next_i<320 and y_pos_next_i<240) or (x_pos_next_i>320 and y_pos_next_i>240)) else "000";
+    green_next_o_2 <= "111" when (x_pos_next_i>320) else "000";
+    blue_next_o_2 <= "11" when (y_pos_next_i>240) else "00";
+	 
+	 red_next_o_3 <= "111" when (x_pos_next_i<128 or x_pos_next_i>384) else "000";
+    green_next_o_3 <= "111" when (x_pos_next_i>256 and x_pos_next_i<512) else "000";
+    blue_next_o_3 <= "11" when ((x_pos_next_i>128 and x_pos_next_i<256) or (x_pos_next_i>512)) else "00";
+	 
+	 red_next_o_4 <= "111" when (y_pos_next_i<160 or y_pos_next_i>320) else "000";
+    green_next_o_4 <= "111" when (y_pos_next_i<160 or y_pos_next_i>320) else "000";
+    blue_next_o_4 <= "11" when (y_pos_next_i<160 or y_pos_next_i>320) else "00";
+	 
+	 red_next_o <= red_next_o_1 when selpat_i(1 downto 0)="00" else 
+						red_next_o_2 when selpat_i(1 downto 0)="01" else
+						red_next_o_3 when selpat_i(1 downto 0)="10" else 
+						red_next_o_4;
+						
+    green_next_o <= green_next_o_1 when selpat_i(1 downto 0)="00" else 
+						  green_next_o_2 when selpat_i(1 downto 0)="01" else
+						  green_next_o_3 when selpat_i(1 downto 0)="10" else
+						  green_next_o_4;
+						  
+    blue_next_o <= blue_next_o_1 when selpat_i(1 downto 0)="00" else
+						 blue_next_o_2 when selpat_i(1 downto 0)="01" else
+						 blue_next_o_3 when selpat_i(1 downto 0)="10" else
+						 blue_next_o_4;
 end Behavioral;
