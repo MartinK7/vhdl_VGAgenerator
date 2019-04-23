a9201bbd5cb8e785c5240bc7dfb3fb33c6443c1e
MartinK7 <unicorn.light.bulb@gmail.com>
2019-04-23 20:54:32 +0200

    Pattern upgrade, grammer =D

--- vga_generator.vhd
+++ vga_generator.vhd
@@ -23,8 +23,8 @@ entity vga_generator is
 		  blue_next_i   : in std_logic_vector(2-1 downto 0); -- future BLUE color
 
         -- Entity output signals
-        h_sinc_o : out std_logic;
-        v_sinc_o : out std_logic;
+        h_sync_o : out std_logic;
+        v_sync_o : out std_logic;
 		  red_o    : out std_logic_vector(3-1 downto 0); -- future RED color
 		  green_o  : out std_logic_vector(3-1 downto 0); -- future GREEN color
 		  blue_o   : out std_logic_vector(2-1 downto 0); -- future BLUE color		  
@@ -55,10 +55,10 @@ architecture Behavioral of vga_generator is
 	 signal line_cnt : std_logic_vector(10-1 downto 0);
     signal pixel_cnt_next : std_logic_vector(10-1 downto 0);
 	 signal line_cnt_next : std_logic_vector(10-1 downto 0);
-	 signal h_sinc_next : std_logic;
-	 signal h_sinc : std_logic;
-	 signal v_sinc_next : std_logic;
-	 signal v_sinc : std_logic;
+	 signal h_sync_next : std_logic;
+	 signal h_sync : std_logic;
+	 signal v_sync_next : std_logic;
+	 signal v_sync : std_logic;
 	 signal red_next   : std_logic_vector(3-1 downto 0); -- future RED color (masked lines, sync)
 	 signal green_next : std_logic_vector(3-1 downto 0); -- future GREEN color (masked lines, sync)
 	 signal blue_next  : std_logic_vector(2-1 downto 0); -- future BLUE color (masked lines, sync)
@@ -73,13 +73,13 @@ begin
             if rst_n_i = '0' then             -- synchronous reset
                 pixel_cnt <= (others => '0'); -- clear all bits in register
                 line_cnt <= (others => '0');  -- clear all bits in register
-					 h_sinc <= '1';
-					 v_sinc <= '1';
+					 h_sync <= '1';
+					 v_sync <= '1';
             else
                 pixel_cnt <= pixel_cnt_next;  -- update register value
                 line_cnt <= line_cnt_next;    -- update register value
-					 h_sinc <= h_sinc_next;
-					 v_sinc <= v_sinc_next;
+					 h_sync <= h_sync_next;
+					 v_sync <= v_sync_next;
 					 red_o <= red_next;
 					 green_o <= green_next;
 					 blue_o <= blue_next;
@@ -97,13 +97,13 @@ begin
                       line_cnt + 1 when (pixel_cnt=TOTAL_PIXELS-1 and line_cnt/=TOTAL_LINES-1) else
 							 line_cnt;
 							 
-	 h_sinc_next <= '0' when pixel_cnt=VISIBLE_AREA_PIXELS+FRONT_PORCH_PIXELS else
+	 h_sync_next <= '0' when pixel_cnt=VISIBLE_AREA_PIXELS+FRONT_PORCH_PIXELS else
 	                '1' when pixel_cnt=VISIBLE_AREA_PIXELS+FRONT_PORCH_PIXELS+SYNC_PULSE_PIXELS else
-						 h_sinc;
+						 h_sync;
 						 
-	 v_sinc_next <= '0' when line_cnt=VISIBLE_AREA_LINES+FRONT_PORCH_LINES else
+	 v_sync_next <= '0' when line_cnt=VISIBLE_AREA_LINES+FRONT_PORCH_LINES else
 	                '1' when line_cnt=VISIBLE_AREA_LINES+FRONT_PORCH_LINES+SYNC_PULSE_LINES else
-						 v_sinc;
+						 v_sync;
 						 
 	 red_next <= "000" when ((pixel_cnt_next>=VISIBLE_AREA_PIXELS and pixel_cnt_next<TOTAL_PIXELS) or (line_cnt_next>=VISIBLE_AREA_LINES and line_cnt_next<TOTAL_LINES)) else red_next_i;
 	 green_next <= "000" when ((pixel_cnt_next>=VISIBLE_AREA_PIXELS and pixel_cnt_next<TOTAL_PIXELS) or (line_cnt_next>=VISIBLE_AREA_LINES and line_cnt_next<TOTAL_LINES)) else green_next_i;
@@ -112,8 +112,8 @@ begin
     --------------------------------------------------------------------------------
     -- Output logic
     --------------------------------------------------------------------------------
-	 h_sinc_o <= h_sinc;
-	 v_sinc_o <= v_sinc;
+	 h_sync_o <= h_sync;
+	 v_sync_o <= v_sync;
 	 x_pos_next_o <= pixel_cnt_next;
 	 y_pos_next_o <= line_cnt_next;
 end Behavioral;
